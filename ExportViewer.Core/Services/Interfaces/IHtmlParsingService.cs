5abab4c7cb585843c2bd4875fa0621160cdbdaeb
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-09-06 03:22:40 +0200

    Added interfaces for HTML and JSON parsing service

---
+++ ExportViewer.Core/Services/Interfaces/IHtmlParsingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using ExportViewer.Core.Models.HTML;
+using ExportViewer.Core.Models.Interfaces;
+
+namespace ExportViewer.Core.Services.Interfaces
+{
+    public interface IHtmlParsingService
+    {
+        public Task<IEnumerable<IMessage>> GetMessages(string filePath, CultureInfo locale, string exportLocation);
+    }
+}
---
+++ ExportViewer.Core/Services/Interfaces/IJsonParsingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using ExportViewer.Core.Models.Interfaces;
+using ExportViewer.Core.Models.JSON;
+
+namespace ExportViewer.Core.Services.Interfaces
+{
+    public interface IJsonParsingService
+    {
+        public Task<IEnumerable<IMessage>> GetMessages(string filePath, CultureInfo locale, string exportLocation);
+    }
+}
