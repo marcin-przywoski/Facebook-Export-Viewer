cba5c78f3006c7379c594891fff1228b3da19162
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-09-06 03:31:36 +0200

    Added models for HTML and JSON export messages

---
+++ ExportViewer.Core/Models/JSON/Reaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ExportViewer.Core.Models.JSON
+{
+    public class Reaction
+    {
+        [JsonProperty(PropertyName = "reaction")]
+        public string ReactionType { get; set; }
+
+        [JsonProperty(PropertyName = "actor")]
+        public string Actor { get; set; }
+    }
+}
