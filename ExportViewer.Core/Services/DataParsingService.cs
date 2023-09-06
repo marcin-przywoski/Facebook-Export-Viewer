944ccb0145bf0b44f72000086a18dbef786334fe
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-09-06 03:33:23 +0200

    Added DataParsingService

---
+++ ExportViewer.Core/Services/DataParsingService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ExportViewer.Core.Enums;
+using ExportViewer.Core.Models.Interfaces;
+using ExportViewer.Core.Services.Interfaces;
+
+namespace ExportViewer.Core.Services
+{
+    public class DataParsingService : IDataParsingService
+    {
+        public Task<IEnumerable<IMessage>> GetExportFileMessages (string exportLocation , string exportFileLocation , ExportType type , CultureInfo locale , IProgress<string> progress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetExportFiles (string exportLocation , ExportType type , IProgress<string> progress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CultureInfo> GetExportLanguage (string exportLocation , ExportType type , IProgress<string> progress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ExportType> GetExportType (string exportLocation , IProgress<string> progress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
