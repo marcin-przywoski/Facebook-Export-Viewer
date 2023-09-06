fb47a8a810e06d8a1296a55002dd9bebbb46395b
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-09-06 03:20:39 +0200

    Added interface for DateEmbeddingService

---
+++ ExportViewer.Core/Services/Interfaces/IDateEmbeddingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ExportViewer.Core.Models.Interfaces;
+
+namespace ExportViewer.Core.Services.Interfaces
+{
+    public interface IDateEmbeddingService
+    {
+        public Task EmbeddDate(IMessage message, string exportLocation, string destinationPath, IProgress<string> progress);
+    }
+}
