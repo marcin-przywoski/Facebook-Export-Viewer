cc8998a36b91bb0252ce8699f084ef66661ace88
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-09-06 03:19:38 +0200

    Added interface for DataParsingService

---
+++ ExportViewer.Core/Services/Interfaces/IDataParsingService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using ExportViewer.Core.Enums;
+using ExportViewer.Core.Models.Interfaces;
+
+namespace ExportViewer.Core.Services.Interfaces
+{
+    public interface IDataParsingService
+    {
+        public Task<CultureInfo> GetExportLanguage(string exportLocation, ExportType type, IProgress<string> progress);
+
+        public Task<IEnumerable<IMessage>> GetExportFileMessages(string exportLocation, string exportFileLocation, ExportType type, CultureInfo locale, IProgress<string> progress);
+
+        public Task<ExportType> GetExportType(string exportLocation, IProgress<string> progress);
+
+        public Task<IEnumerable<string>> GetExportFiles(string exportLocation, ExportType type, IProgress<string> progress);
+
+    }
+}
