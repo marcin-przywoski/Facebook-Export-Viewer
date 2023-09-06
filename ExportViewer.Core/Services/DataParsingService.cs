8317de1893fb0857d1d15fcec0f39beb1270fec8
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-09-06 03:37:15 +0200

    Added implementation of GetExportType

--- ExportViewer.Core/Services/DataParsingService.cs
+++ ExportViewer.Core/Services/DataParsingService.cs
@@ -29,9 +29,33 @@ namespace ExportViewer.Core.Services
             throw new NotImplementedException();
         }
 
-        public Task<ExportType> GetExportType (string exportLocation , IProgress<string> progress)
+
+
+        public Task<ExportType> GetExportType(string exportLocation, IProgress<string> progress)
         {
-            throw new NotImplementedException();
+            string jsonSearchPattern = "*.json";
+            string htmlSearchPattern = "*.html";
+
+            bool hasJsonFiles = Directory.EnumerateFiles(exportLocation, jsonSearchPattern, SearchOption.AllDirectories).Any();
+            bool hasHtmlFiles = Directory.EnumerateFiles(exportLocation, htmlSearchPattern, SearchOption.AllDirectories).Any();
+
+            if (hasJsonFiles)
+            {
+                progress.Report($"Export type: JSON");
+                return Task.FromResult(ExportType.Json);
+            }
+            else if (hasHtmlFiles)
+            {
+                progress.Report($"Export type: HTML");
+                return Task.FromResult(ExportType.HTML);
+            }
+            else
+        {
+                progress.Report($"Export type: HTML");
+                return Task.FromResult(ExportType.NotApplicable);
+            }
         }
+
+
     }
 }
