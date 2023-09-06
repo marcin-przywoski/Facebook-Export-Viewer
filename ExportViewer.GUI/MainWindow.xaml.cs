6bbe12847a8f67665a23d6e7e9c0e6a613505caf
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-09-07 01:34:57 +0200

    Added implementation of event handlers

--- ExportViewer.GUI/MainWindow.xaml.cs
+++ ExportViewer.GUI/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -16,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ExportViewer.Core.Models.Interfaces;
 using ExportViewer.Core.Services.Interfaces;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace ExportViewer.GUI
 {
@@ -24,6 +25,9 @@ namespace ExportViewer.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        string exportLocation;
+        string destination;
+
         private readonly IHtmlParsingService htmlParsingService;
         private readonly IJsonParsingService jsonParsingService;
         private readonly IDataParsingService dataParsingService;
@@ -38,19 +42,70 @@ namespace ExportViewer.GUI
             this.jsonParsingService = jsonParsingService;
         }
 
-        private void StartButton_Click (object sender , RoutedEventArgs e)
+        private void DestinationLocation_Click(object sender, RoutedEventArgs e)
         {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok && dialog.FileName != null)
+            {
+                DestinationLocationLabel.Content = "Destination location : ";
+                destination = dialog.FileName + "/";
+                DestinationLocationLabel.Content += destination;
+                DestinationLocationLabel.ToolTip = DestinationLocationLabel.Content;
+            }
 
         }
 
-        private void DestinationLocation_Click (object sender , RoutedEventArgs e)
+        private void SourceLocation_Click(object sender, RoutedEventArgs e)
         {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok && dialog.FileName != null)
+            {
+                SourceLocationLabel.Content = "Source location : ";
+                exportLocation = dialog.FileName + "/";
+                SourceLocationLabel.Content += exportLocation;
+                SourceLocationLabel.ToolTip = SourceLocationLabel.Content;
+            }
 
         }
 
-        private void SourceLocation_Click (object sender , RoutedEventArgs e)
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
         {
+            var progress = new Progress<string>(x => { OutputLog.AppendText(x + "\n"); OutputLog.ScrollToEnd(); });
 
+
+            if (String.IsNullOrEmpty(destination) || String.IsNullOrEmpty(exportLocation))
+            {
+                MessageBox.Show("One of the paths have not been selected!");
+        }
+            else
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+
+                var exportType = await dataParsingService.GetExportType(exportLocation, progress);
+
+                var exportLanguage = await dataParsingService.GetExportLanguage(exportLocation, exportType, progress);
+
+                var exportFiles = await dataParsingService.GetExportFiles(exportLocation, exportType, progress);
+
+                var messages = new List<IMessage>();
+
+                foreach (var file in exportFiles)
+                {
+                    messages.AddRange(await Task.Run(() => dataParsingService.GetExportFileMessages(exportLocation, file, exportType, exportLanguage, progress)));
+                }
+
+                await Task.WhenAll(messages.Select(x => Task.Run(() => dateEmbeddingService.EmbeddDate(x, exportLocation, destination, progress))));
+
+                stopwatch.Stop();
+                OutputLog.AppendText($"Time elapsed total: {stopwatch.Elapsed:g}." + "\n" + $"Log saved to {destination} log.txt");
+                await File.WriteAllTextAsync(destination + "log.txt", OutputLog.Text);
+            }
         }
     }
 }
