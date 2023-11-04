c46f3f0cd5573072c450008766ed66a920056387
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-11-04 23:52:56 +0100

    Added IUtilities interface for interacting with Utilities View

---
+++ ExportViewer.GUI/Interfaces/IUtilities.cs
@@ -0,0 +1,12 @@
+namespace ExportViewer.GUI.Interfaces
+{
+    public interface IUtilities
+    {
+        public void SelectDestination (object sender , System.EventArgs e);
+
+        public void SelectSource (object sender , System.EventArgs e);
+
+        public void Start (object sender , System.EventArgs e);
+
+    }
+}
