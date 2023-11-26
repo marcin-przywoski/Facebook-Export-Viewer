109b2ef4b83e2f4f67dfcd6043713fd2c38b26e5
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-11-27 00:15:28 +0100

    Added more functionality to Utilities.xaml

--- ExportViewer.GUI/Views/Utilities.xaml.cs
+++ ExportViewer.GUI/Views/Utilities.xaml.cs
@@ -10,17 +10,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ExportViewer.GUI.Interfaces;
+using ExportViewer.GUI.ViewModels;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace ExportViewer.GUI.Views
 {
     /// <summary>
     /// Interaction logic for Utilities.xaml
     /// </summary>
-    public partial class Utilities : UserControl
+    public partial class Utilities : UserControl, IUtilities
     {
         public Utilities ()
         {
             InitializeComponent();
+
+            var utilitiesViewModel = DataContext as UtilitiesViewModel;
+
+            if (utilitiesViewModel != null)
+                utilitiesViewModel.Utilities = this;
+
+        }
+
+        public void SelectDestination()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok && dialog.FileName != null)
+            {
+                DestinationLocation.Text = dialog.FileName + "/";
+
+            }
+        }
+
+        public void SelectSource()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok && dialog.FileName != null)
+            {
+                SourceLocation.Text = dialog.FileName + "/";
+            }
+        }
+
+        public void ShowSourceLocation(string sourceLocation)
+        {
+            MessageBox.Show($"Source location is: {sourceLocation}");
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void StartButton_Click (object sender , RoutedEventArgs e)
+        {
+
         }
     }
 }
