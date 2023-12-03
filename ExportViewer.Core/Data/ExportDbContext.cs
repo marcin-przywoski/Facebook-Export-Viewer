fefbaa377ef9d79b46f2914132aae5d2b3374ad7
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-12-03 20:51:14 +0100

    Added ExportDbContext

---
+++ ExportViewer.Core/Data/ExportDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExportViewer.Core.Models.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExportViewer.Core.Data
+{
+    public class ExportDbContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+
+
+
+        public ExportDbContext (DbContextOptions<ExportDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
