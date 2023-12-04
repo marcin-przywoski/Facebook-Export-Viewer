24f93a9e39e6fff111cd90baf1f371b960e14d2a
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-12-05 00:32:34 +0100

    Added project for benchmarking the performance using Benchmark.NET

---
+++ ExportViewer.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExportViewer.Benchmarks
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
