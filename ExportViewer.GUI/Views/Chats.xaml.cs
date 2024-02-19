75f7be5860929ab9ee0a233d948881ba44fa5b6c
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2024-02-20 00:43:26 +0100

    Added Chats.xaml view

---
+++ ExportViewer.GUI/Views/Chats.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExportViewer.GUI.Views
+{
+    /// <summary>
+    /// Interaction logic for Chats.xaml
+    /// </summary>
+    public partial class Chats : UserControl
+    {
+        public Chats ()
+        {
+            InitializeComponent();
+        }
+    }
+}
