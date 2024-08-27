92f1a1d1611ad2e2e79bc078e601ef1c5908f882
Marcin Przywóski <marcin.przywoski26@gmail.com>
2024-08-27 22:27:19 +0200

    Added Benchmark project to solution

---
+++ ExportViewer.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExportViewer.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
