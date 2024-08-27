a4aa82eb4d22159d38108f9c2f6244326f8636d6
Marcin Przywóski <marcin.przywoski26@gmail.com>
2024-08-27 23:19:27 +0200

    Added unit tests for getting export type

---
+++ ExportViewer.Tests/DataParsingService_Tests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using ExportViewer.Core.Enums;
+using ExportViewer.Core.Services;
+using Xunit;
+
+namespace ExportViewer.Tests
+{
+    public class DataParsingService_Tests
+    {
+        private readonly DataParsingService _dataParsingService;
+
+        private readonly IProgress<string> _progress = new Progress<string>(s => Console.WriteLine(s));
+
+        public DataParsingService_Tests ()
+        {
+            _dataParsingService = new DataParsingService();
+        }
+
+        [Fact]
+        public async void GetExportType_ReturnsJson ()
+        {
+            // Arrange
+            string tempDir = Path.Combine(Path.GetTempPath() , Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempDir);
+
+            string jsonFilePath = Path.Combine(tempDir , "test.json");
+            if (!File.Exists(jsonFilePath))
+            {
+                File.Create(jsonFilePath);
+            }
+
+            // Act
+            ExportType result = await _dataParsingService.GetExportType(tempDir , _progress);
+
+            // Assert
+            Assert.Equal(ExportType.Json , result);
+        }
+
+        [Fact]
+        public async void GetExportType_ReturnsHtml ()
+        {
+            // Arrange
+            string tempDir = Path.Combine(Path.GetTempPath() , Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempDir);
+
+            string htmlFilePath = Path.Combine(tempDir , "test.html");
+            if (!File.Exists(htmlFilePath))
+            {
+                File.Create(htmlFilePath);
+            }
+
+            // Act
+            ExportType result = await _dataParsingService.GetExportType(tempDir , _progress);
+
+            // Assert
+            Assert.Equal(ExportType.HTML , result);
+        }
+    }
+}
