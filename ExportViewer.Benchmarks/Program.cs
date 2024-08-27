266c33f5b01dce7ea9ca1dceac4b22294fb3fad5
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2024-08-27 22:50:15 +0200

    Added Benchmark project
    * Added Benchmark project to solution
    
    * Added BenchmarkDotNet NuGet reference
    
    * Added sample benchmark
    
    ---------
    
    Co-authored-by: Marcin Przywóski <marcin.przywoski26@gmail.com>

---
+++ ExportViewer.Benchmarks/Program.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ExportViewer.Benchmarks
+{
+    public class Program
+    {
+        static void Main (string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Program>();
+        }
+
+        private const string TestString = "Hello, World!";
+
+        [Benchmark]
+        public void UseStringBuilder ()
+        {
+            var sb = new System.Text.StringBuilder();
+            for (int i = 0; i < 1000; i++)
+            {
+                sb.Append(TestString);
+            }
+            
+            _ = sb.ToString();
+        }
+
+        [Benchmark]
+        public void UseStringConcatenation ()
+        {
+            string result = "";
+            for (int i = 0; i < 1000; i++)
+            {
+                result += TestString;
+            }
+        }
+    }
+}
