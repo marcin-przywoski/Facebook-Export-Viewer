c66479b3c65bbb838b36bf4e1799f463298741f2
Marcin Przywoski <bluza26@gmail.com>
2024-10-06 00:43:40 +0200

    Added blank AvaloniaUI project

---
+++ ExportViewer.GUI.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ExportViewer.GUI.Avalonia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
