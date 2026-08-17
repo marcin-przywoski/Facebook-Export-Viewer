2dad71e85ce077253589775a5d902fa0a64324d2
Marcin Przywóski <marcin.przywoski26@gmail.com>
2026-08-17 21:59:59 +0200

    Added missing import

--- ExportViewer.Core/Services/DateEmbeddingService.cs
+++ ExportViewer.Core/Services/DateEmbeddingService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ExportViewer.Core.Models.Common;
 using ExportViewer.Core.Models.Interfaces;
