f9d2d55bc9e26d4ce5299a4fac8b42781c448432
Marcin Przywóski <marcin.przywoski26@gmail.com>
2026-08-17 21:50:44 +0200

    Added missing import

--- ExportViewer.Core/Services/DataParsingService.cs
+++ ExportViewer.Core/Services/DataParsingService.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using AngleSharp.Html.Parser;
